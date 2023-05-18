ccdaf460c3009515fc1272bf079b1539bfa869e7
Andres Kouvach <akouvach@yahoo.com>
2023-05-18 05:52:22 -0300

    prueba

--- bin/prueba.dart
+++ bin/prueba.dart
@@ -1,10 +1,10 @@
-import 'package:prueba/prueba.dart' as prueba;
+// import 'package:prueba/prueba.dart' as prueba;
 import 'dart:io';
-
 
 void main(List<String> arguments) {
   // print('Hello world: ${prueba.calculate()}!');
   print("Ingrese el nombre del jugador: ");
-var nombre= stdin.readLineSync();
-print("Se ingreso el nombre $nombre");
+  var nombre = stdin.readLineSync()!;
+  stdout.write("su nombre $nombre");
+  // print("Se ingreso el nombre $nombre");
 }
