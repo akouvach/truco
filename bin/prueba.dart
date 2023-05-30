a79140987723a17953ca46a0c2914dda550050d3
Marcos <mkouvach@gmail.com>
2023-05-29 21:09:28 -0300

    hola1

--- bin/prueba.dart
+++ bin/prueba.dart
@@ -1,10 +1,105 @@
+
+import 'dart:ffi';
+
 import 'package:prueba/prueba.dart' as prueba;
 import 'dart:io';
 
 
+
 void main(List<String> arguments) {
-  // print('Hello world: ${prueba.calculate()}!');
+  
+  int i,cont=0,j,cont1=0;
+  var vec_valor=[40,39,38,37,36,36,36,36,32,32,32,32,28,28,26,26,26,26,22,22,22,22,18,18,18,18,14,14,12,12,12,12,8,8,8,8,4,4,4,4];
+  var vec_carta=["1 de espada","1 de basto","7 de espada","7 de oro","3 de espada","3 de oro","3 de basto","3 de copa","2 de copa","2 de basto","2 de espada","2 de oro","1 de copa","1 de oro","12 de copa","12 de espada","12 de oro","12 de basto","11 de basto","11 de oro","11 de espada","11 de copa","10 de copa","10 de basto","10 de oro","10 de espada","7 de basto","7 de copa","6 de oro","6 de copa","6 de espada","6 de basto","5 de espada","5 de oro","5 de basto","5 de copa","4 de copa","4 de basto","4 de oro","4 de espada"];
+  var vec_mazo=["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"];
+  var vec_jug1=["0","0","0"];
+  var vec_jug2=["0","0","0"];
+  var jugadores=[["0","0","0"],["0","0","0"]];
+  var valor_jug1,valor_jug2;
+  print("Bienvenido");
   print("Ingrese el nombre del jugador: ");
-var nombre= stdin.readLineSync();
-print("Se ingreso el nombre $nombre");
+  var nombre= stdin.readLineSync();
+  print("Bienvenido $nombre");
+
+  for(i=0;i<40;i++)
+  {
+    vec_mazo[i]=vec_carta[i]; 
+  }
+  vec_mazo.shuffle();
+
+//Asigno tres cartas a cada jugador
+
+  for(j=0;j<3;j++)
+  {
+    for(i=0;i<jugadores.length;i++)
+    {
+      jugadores[i][j]=vec_mazo[cont];
+      cont++;
+    }
+
+  }
+
+  //Se comparan los valores para saber quien gano
+
+  for(i=0;i<jugadores.length;i++)
+  {
+      for(j=0;j<3;j++)
+      {
+        valor_jug1=Busqueda(vec_carta,vec_valor,jugadores[i][j]);
+        cont1++;
+        valor_jug2=Busqueda(vec_carta,vec_valor,jugadores[cont1][j]);
+      }
+
+
+      if(valor_jug2<valor_jug1)
+      {
+        print("Gano jugador 1");
+      }
+      else
+      {
+        if(valor_jug2==valor_jug1)
+        {
+          print("Empate");
+        }
+        else
+        {
+          print("Gano jugador 2");
+        }
+      }
+
+    cont1=0;
+
+  }
+
+  
+  
 }
+
+int Busqueda( var VC[], var VV[], var x)
+{
+  int val=-1,i=0;
+
+  while(val==-1&&i<VC.length)
+{
+  if(VC[i]==x)
+  {
+    val=VV[i];
+
+  }
+   else
+   {
+     i++;
+
+   }
+
+
+}	
+
+return val;
+
+
+}
+*/
+
+
+
